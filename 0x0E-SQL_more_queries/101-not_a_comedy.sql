3232425ebf7e0575e1773e7c621a4ff59a371a04
Yohanes Gutema <99284608+Yohanes-GR@users.noreply.github.com>
2022-08-11 00:29:04 +0300

    introduction to sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,13 +1,19 @@
--- list all genres not linked to the show Dexter
-   -- Each record should display:
-      -- tv_genres.name
-   -- Results must be sorted in ascending order by the genre name
-   -- The database name will be passed as an argument of the mysql command
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title.
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
 
-SELECT name FROM tv_genres
-WHERE tv_genres.id NOT IN (
-      SELECT tv_genres.id FROM tv_genres
-      JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-      JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-      WHERE tv_shows.title = "DEXTER" )
-ORDER BY tv_genres.name;
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
