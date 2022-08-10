85ea7934f7ca8e5d661f4713cc7df9294cfc90b2
Yohanes Gutema <99284608+Yohanes-GR@users.noreply.github.com>
2022-08-10 23:44:51 +0300

    introduction to sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,13 @@
--- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
---    The tv_genres table contains only one record where name = Comedy (but the id can be different)
---    Each record should display: tv_shows.title
---    Results must be sorted in ascending order by the show title
---    You can use a maximum of two SELECT statement
---    The database name will be passed as an argument of the mysql command
-IGNORE ALL;
+-- list all genres not linked to the show Dexter
+   -- Each record should display:
+      -- tv_genres.name
+   -- Results must be sorted in ascending order by the genre name
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT name FROM tv_genres
+WHERE tv_genres.id NOT IN (
+      SELECT tv_genres.id FROM tv_genres
+      JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+      JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+      WHERE tv_shows.title = "DEXTER" )
+ORDER BY tv_genres.name;
