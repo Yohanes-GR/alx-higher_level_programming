d6c73e14f65a510f35004dc0e262ec949068e6d2
Yohanes-GR <yohgut@gmail.com>
2022-09-05 15:32:36 +0300

    java scripts

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-// prints a square
-
-if (isNaN(process.argv[2])) {
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('X'.repeat(parseInt(process.argv[2])));
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
   }
 }
