6e274c7a6f9602a164ec8f098c713fb62fbba65b
Yohanes-GR <yohgut@gmail.com>
2022-09-05 15:34:47 +0300

    java scripts

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// prints the addition of 2 integers
-
-function add (a, b) {
-  return parseInt(a) + parseInt(b);
-}
-
-console.log(add(process.argv[2], process.argv[3]));
+module.exports = {
+  add: function (a, b) {
+    return a + b;
+  }
+};
