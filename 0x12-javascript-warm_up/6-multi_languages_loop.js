cd44924bf6ab8d6fec401556b92a376513fe97ee
Yohanes-GR <yohgut@gmail.com>
2022-09-06 15:21:07 +0300

    java script

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < strings.length; i++) {
-  console.log(strings[i]);
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < myArr.length; index++) {
+  console.log(myArr[index]);
 }
